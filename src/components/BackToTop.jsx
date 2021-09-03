242a1916d87cda6fcdb6593cf225593c7d86a535
Erick Silva <erick.silva6653@gmail.com>
2021-09-02 21:28:03 -0300

    fix projects page and skillsets tranlate bug, add back to top btn

---
+++ src/components/BackToTop.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+export default function BackToTop() {
+  const [screenPos, setScreenPos] = useState();
+
+  window.onscroll = () => {
+    setScreenPos(window.pageYOffset);
+  }
+  const showIcon = screenPos > 1000 ? 'show' : '';
+  return (
+    <a href="#home" className={`${showIcon} back-top fas fa-chevron-up`} />
+  )
+}
